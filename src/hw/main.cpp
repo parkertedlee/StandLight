135722a047dae72fa790902e3c7c6f3d1ee99169
lcs40112 <lcs40112@naver.com>
2022-09-13 05:20:33 +0000

    fist commit

---
+++ src/hw/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "Button.h"
+#include "Led.h"
+#include <wiringPi.h>
+#include "Listener.h"
+
+
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+
+    Button button1(27);
+    Led led1(25);
+    Listener listener(&button1,&led1);
+
+    while (1)
+    {
+        listener.checkEvent();
+        delay(50);
+    }
+
+    return 0;
+}
